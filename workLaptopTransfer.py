ee653f5b6b18691bbb3423403f9af47cd6ad2fd0
Brett O'Connor <bwo7@cornell.edu>
2024-03-30 21:10:03 -0700

    first push to the master list - tunes.txt.  Also made a script that properly formats the emailed tunes from my work laptop

---
+++ workLaptopTransfer.py
@@ -0,0 +1,34 @@
+# format document properly from work laptop email
+
+import os
+
+INPUT_FOLDER    = 'fromWorkLaptop'
+INPUT_FILE      = 'workLaptopTunes.txt' 
+
+OUTPUT_FILE     = 'formattedWorkLaptopTunes.txt'
+
+categories = ['clubbin', 'chill', 'trance', 'techno']
+
+# read in input file 
+with open(os.path.join(INPUT_FOLDER, INPUT_FILE), 'r') as fr:
+    lines = fr.readlines()
+
+# remove blank lines in between 
+filteredLines = []
+for lineIdx, line in enumerate(lines):
+    if not(line == '\n'):
+        filteredLines.append(line)
+
+# add in a blank line above each category
+outputLines = []
+for filteredLineIdx, filteredLine in enumerate(filteredLines):
+    if filteredLine in categories:
+        addBlank = ['\n', filteredLine]
+        outputLines = outputLines + addBlank
+    else:
+        outputLines.append(filteredLine)
+
+# write to output file
+with open(os.path.join(INPUT_FOLDER, OUTPUT_FILE), 'w') as fw:
+    fw.writelines(outputLines)
+    print('File written to: ', os.path.join(INPUT_FOLDER, OUTPUT_FILE))
